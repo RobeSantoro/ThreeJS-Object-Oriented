3350cb0eb65379fd84bdb43b47d5b98da2fb8ec2
Robe Santoro z800 <robe.santoro@live.com>
2022-03-01 23:12:10 +0100

    Updated Model, Added Draco, Added Animations

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+
 
 class World {
   constructor() {
@@ -8,7 +10,7 @@ class World {
   }
 
   init() {
-    this.webgl = new THREE.WebGLRenderer({ antialias: true});
+    this.webgl = new THREE.WebGLRenderer({ antialias: true });
     this.webgl.outputEncoding = THREE.sRGBEncoding;
     this.webgl.shadowMap.enabled = true;
     this.webgl.shadowMap.type = THREE.PCFSoftShadowMap;
@@ -21,7 +23,7 @@ class World {
     window.addEventListener('resize', () => {
       this.onWindowResize();
     }, false);
-    
+
     //Set the camera
     const fov = 75;
     const aspect = window.innerWidth / window.innerHeight;
@@ -32,12 +34,12 @@ class World {
 
     // Create the scene
     this.scene = new THREE.Scene();
-    
+
     // Add a directional light
     let DirectionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
     DirectionalLight.position.set(2, 3, 2);
     DirectionalLight.target.position.set(0, 0, 0);
-    DirectionalLight.castShadow = true;    
+    DirectionalLight.castShadow = true;
     DirectionalLight.shadow.mapsize = new THREE.Vector2(2048, 2048);
     DirectionalLight.shadow.camera.near = 0.01;
     DirectionalLight.shadow.camera.far = 10.0;
@@ -52,7 +54,7 @@ class World {
     // Add Light helper
     const helper = new THREE.DirectionalLightHelper(DirectionalLight, 1);
     this.scene.add(helper);
-  
+
     // Add an ambient light    
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
     this.scene.add(ambientLight);
@@ -71,12 +73,12 @@ class World {
       './resources/textures/env/posy.jpg',
       './resources/textures/env/negy.jpg',
       './resources/textures/env/posz.jpg',
-      './resources/textures/env/negz.jpg'      
+      './resources/textures/env/negz.jpg'
     ]);
     this.scene.background = envTexture;
 
     // Add ground plane
-    const planeGeometry = new THREE.PlaneGeometry(100 , 100, 1, 1);
+    const planeGeometry = new THREE.PlaneGeometry(100, 100, 1, 1);
     const planeMaterial = new THREE.MeshStandardMaterial({
       color: 0x555555,
       roughness: 0.0,
@@ -88,34 +90,43 @@ class World {
     plane.receiveShadow = true;
     this.scene.add(plane);
 
+
+    this.clock = new THREE.Clock()
+    this.lastElapsedTime = 0
+
     this.loadModel();
     this.animate();
   }
 
   loadModel() {
-    
+
     const DiffuseTexture = new THREE.TextureLoader()
-    .load('./resources/textures/Avatar_Diffuse.jpg', (texture) => {
-      texture.encoding = THREE.sRGBEncoding;
-      texture.needsUpdate = true;
-      texture.flipY = false;
-    });
+      .load('./resources/textures/Avatar_Diffuse.jpg', (texture) => {
+        texture.encoding = THREE.sRGBEncoding;
+        texture.needsUpdate = true;
+        texture.flipY = false;
+      });
 
     const NormalTexture = new THREE.TextureLoader()
-    .load('./resources/textures/Avatar_Normal.jpg', (texture) => {
-      texture.needsUpdate = true;
-      texture.flipY = false;      
-    });
+      .load('./resources/textures/Avatar_Normal.jpg', (texture) => {
+        texture.needsUpdate = true;
+        texture.flipY = false;
+      });
 
-    const loader = new GLTFLoader();
-    loader.load('./resources/models/Avatar.glb', (gltf) => {
-      
+    // Draco loader
+    const dracoLoader = new DRACOLoader()
+    dracoLoader.setDecoderPath('./decoder/')
+
+    const gltfLoader = new GLTFLoader()
+    gltfLoader.setDRACOLoader(dracoLoader)
+    gltfLoader.load('./resources/models/Avatar.glb', (gltf) => {
+
       const model = gltf.scene;
 
       // Cast shadows
       model.traverse((child) => {
         if (child.isMesh) {
-        
+
           child.castShadow = true;
           child.material.map = DiffuseTexture;
           child.material.normalMap = NormalTexture;
@@ -126,7 +137,18 @@ class World {
 
       // Add the model to the scene
       this.scene.add(gltf.scene);
-    });
+
+      // Add Animations
+      this.animations = gltf.animations;
+      this.mixer = new THREE.AnimationMixer(model);
+      this.mixer.clipAction(this.animations[3]).play()
+    },
+      (xhr) => {
+        console.log(/* (xhr.loaded / xhr.total * 100) + '% loaded' */);
+      },
+      (error) => {
+        console.log(error);
+      })
   }
 
   onWindowResize() {
@@ -139,6 +161,14 @@ class World {
     requestAnimationFrame(() => {
       this.webgl.render(this.scene, this.camera);
       this.orbitControls.update();
+
+      const elapsedTime = this.clock.getElapsedTime()
+      const deltaTime = elapsedTime - this.lastElapsedTime
+      this.lastElapsedTime = elapsedTime
+
+      if (this.mixer) {
+        this.mixer.update(deltaTime);
+      }
       this.animate();
     })
   }
